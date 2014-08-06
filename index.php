d83eb69bdc2e1c8bb2d78c56c2a044d732562100
孙定涛 <570551850@qq.com>
2014-08-06 21:59:00 +0800

    add index.pho

---
+++ index.php
@@ -0,0 +1,365 @@
+<!-- <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>湖北天地-互联网产品体验解决机构</title>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <link href="Assets/CSS/Master.css" rel="stylesheet" type="text/css" />
+    <link href="Assets/CSS/IE8.css" rel="stylesheet" type="text/css" />
+
+    <!--<script type="text/javascript" src="Assets/JS/hsx3hhh.js"></script>
+-->
+<!--<script type="text/javascript">try { Typekit.load(); } catch (e) { }</script>-->
+
+</head>
+<body>
+<!-- form data-->
+<form method="post" action="" id="form1">
+<!--  <div class="aspNetHidden">
+    <input type="hidden" name="__EVENTTARGET" id="__EVENTTARGET" value="" />
+    <input type="hidden" name="__EVENTARGUMENT" id="__EVENTARGUMENT" value="" />
+    <input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="/wEPDwUJNDAyODUxMTQ2D2QWAmYPZBYEAgEPZBYCAgIPFgIeBFRleHQFRDxsaW5rIGhyZWY9Ii9Bc3NldHMvQ1NTL0lFOC5jc3MiIHJlbD0ic3R5bGVzaGVldCIgdHlwZT0idGV4dC9jc3MiIC8+ZAIDD2QWBAIJD2QWCgIBDw8WAh4LTmF2aWdhdGVVcmwFHmh0dHA6Ly93d3cubW9iZWRpYS5jby51ay93b3JrL2RkAgMPDxYCHwEFHmh0dHA6Ly93d3cubW9iZWRpYS5jby51ay90ZWFtL2RkAgUPDxYCHwEFIWh0dHA6Ly93d3cubW9iZWRpYS5jby51ay9jbGllbnRzL2RkAgcPDxYCHwEFHmh0dHA6Ly93d3cubW9iZWRpYS5jby51ay9uZXdzL2RkAgkPDxYCHwEFIWh0dHA6Ly93d3cubW9iZWRpYS5jby51ay9jb250YWN0L2RkAgsPZBYEAhUPZBYCAgMPFCsAAmRkZAIZD2QWAgIDDxQrAAJkZGQYAgUeY3RsMDAkTWFpbkNvbnRlbnQkbHZMYXRlc3RCbG9nD2dkBRhjdGwwMCRNYWluQ29udGVudCRsdldvcmsPZ2Q5q1fxIhPUFXbAQMFOVx+DPfKI7f/XggTgkTg/X4iBUg==" />
+</div>
+-->
+<script type="text/javascript">
+//<![CDATA[
+// var theForm = document.forms['form1'];
+// if (!theForm) {
+//     theForm = document.form1;
+// }
+// function __doPostBack(eventTarget, eventArgument) {
+//     if (!theForm.onsubmit || (theForm.onsubmit() != false)) {
+//         theForm.__EVENTTARGET.value = eventTarget;
+//         theForm.__EVENTARGUMENT.value = eventArgument;
+//         theForm.submit();
+//     }
+// }
+//]]>
+</script>
+
+<!--<script src="Web.js" type="text/javascript"></script>
+-->
+<!--<script src="http://www.mobedia.co.uk/ScriptResource.axd?d=UlhKBwUZ-6rPvZ_eLxEqzmb3BgKi4aYAUnBEuy6oZEKetRZWuzEu2Zp-QdcvbPj66FoeWQnQ0vKLR3PQmr7qD5rMy_od0fuxO38KFTlHQPpseghjSa3Mgjcan3opR9W6U_WaClw1b1iBPb_7IqpvNg2&t=ffffffffdd783992" type="text/javascript"></script>
+-->
+<script type="text/javascript">
+//<![CDATA[
+//if (typeof(Sys) === 'undefined') throw new Error('ASP.NET Ajax client-side framework failed to load.');
+//]]>
+</script>
+
+<!--<script src="http://www.mobedia.co.uk/ScriptResource.axd?d=uzd5dKa1bFKARolSSm_GOf290llS48_RaO41SZotEc9_gSaughVxECqLLpnx4Kvtevkftb9SUw6r_mUE80Nx8gaz8TelSUnU84LxHBIAvacRoPjVIBdAcSIHryamMSaOf5GEcOfWGz4g435sr9Vb8Q2&t=ffffffffdd783992" type="text/javascript"></script>
+-->
+<script src="Assets/JS/jquery.min.js" type="text/javascript"></script>
+<script src="Assets/JS/jquery.bbq.min.js" type="text/javascript"></script>
+<script src="Assets/JS/jquery.carousel.pause.js" type="text/javascript"></script>
+<script src="Assets/JS/jquery.easing.js" type="text/javascript"></script>
+<script src="Assets/JS/1.8/jquery-ui.min.js" type="text/javascript"></script>
+<script src="Assets/JS/Master.js" type="text/javascript"></script>
+<!-- <div class="aspNetHidden">
+
+<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="/wEdAAUpGWUc8rPgQJEBubvG3+5PvVsqDhUaaYebrEwXputb6hV/pOs54cZXHDi3NOd8ODj8okOee4fWQHAYju1cSr5XxwSgbcHmIG9NOlwYv3/aQ6R8ayIS/DZqSC+xDDgepFOVSFxzNEbne1RiK0ZjHDIY" />
+</div> -->
+<script type="text/javascript">
+//<![CDATA[
+//Sys.WebForms.PageRequestManager._initialize('ctl00$smMaster', 'form1', ['tctl00$upNewsletter','upNewsletter'], [], [], 90, 'ctl00');
+//]]>
+</script>
+
+<!-- <input type="hidden" name="ctl00$hdfIE" id="hdfIE" value="1" /> -->
+<!--reduce-->
+<!-- <input type="hidden" name="ctl00$hdfDomain" id="hdfDomain" value="http://www.mobedia.co.uk" /> -->
+
+<!-- 顶部栏目 -->
+<div id="Header">
+    <div class="Wrapper">
+        <a id="lnkLogo" class="Logo" href="#" target="_self">
+            <img id="imgLogo" src="Assets/IMG/logo.gif" alt="湖北天地设计机构（t-design）" />
+        </a>
+        <ul id="MainMenu">
+            <li> <a id="lnkWork" href="work/work1.html" target="_self">作品</a> </li> 
+            <li> <a id="lnkWork" href="tool/tool1.html" target="_self">工具插件</a> </li> 
+            <li> <a id="lnkClients" href="www.t-design.cn" target="_self">博客</a> </li> 
+            <!--<li> <a id="lnkNews" href="news1/news.html" target="_self">客户</a> </li> --> 
+            <li class="Last"> <a id="lnkContact" href="contact/contact.html" target="_self">关于我</a> </li>
+        </ul>
+        <div class="Clear"></div>
+    </div>
+</div>
+<!-- ==顶部栏目== -->
+<!-- 内容区域 -->
+<div id="PageContent">
+    <div class="HomeSlideshowState Hidden">Playing</div>
+    <div class="HomeSlideshow">
+        <ul>
+            <li class="Slide1">
+                <a id="MainContent_lnkSlide1" href="work/detail/simon-simon-e-commerce-solution.html" target="_self">
+                    <span class="Hidden">诚信网站</span>
+                </a>
+            </li>
+            <li class="Slide2">
+                <a id="MainContent_lnkSlide2" href="work/detail/kia-ceed-uk-europe.html" target="_self">
+                    <span class="Hidden">曼卡龙珠宝</span>
+                </a>
+            </li>
+            <li class="Slide3">
+                <a id="MainContent_lnkSlide3" href="work/detail/peckforton-castle-ios-app.html" target="_self">
+                    <span class="Hidden">曼卡龙珠宝</span>
+                </a>
+            </li>
+            <li class="Slide4">
+                <a id="MainContent_lnkSlide4" href="work/detail/glenfiddich.html" target="_self">
+                    <span class="Hidden">淘宝网</span>
+                </a>
+            </li>
+            <li class="Slide5">
+                <a id="MainContent_lnkSlide5" href="Contact/contact.html" target="_self">
+                    <span class="Hidden">搜狐网</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="Wrapper">
+        <div class="HomeSlideshowControls">
+            <ul>
+                <li class="First">
+                    <a id="MainContent_lnkPagination1" class="1 Active" href="javascript:void(0);" target="_self">
+                        <span class="Arrow Active"></span>
+                        <span class="Inner Active">
+                            <img id="MainContent_imgPagination1" src="Assets/IMG/IconBeleiveSite.png" alt="诚信网站" />
+                        </span>
+                    </a>
+                </li>
+                <li class="Spacer"></li>
+                <li>
+                    <a id="MainContent_lnkPagination2" class="2" href="javascript:void(0);" target="_self">
+                        <span class="Arrow "></span>
+                        <span class="Inner ">
+                            <img id="MainContent_imgPagination2" src="Assets/IMG/IconZhiduobao.png" alt="Kia" />
+                        </span>
+                    </a>
+                </li>
+                <li class="Spacer"></li>
+                <li>
+                    <a id="MainContent_lnkPagination3" class="3" href="javascript:void(0);" target="_self">
+                        <span class="Arrow"></span>
+                        <span class="Inner">
+                            <img id="MainContent_imgPagination3" src="Assets/IMG/IconMankalong.png" alt="Peckforton Castle" />
+                        </span>
+                    </a>
+                </li>
+                <li class="Spacer"></li>
+                <li>
+                    <a id="MainContent_lnkPagination4" class="4 " href="javascript:void(0);" target="_self">
+                        <span class="Arrow"></span>
+                        <span class="Inner">
+                            <img id="MainContent_imgPagination4" src="Assets/IMG/IconTaobao.png" alt="Glenfiddich" />
+                        </span>
+                    </a>
+                </li>
+                <li class="Spacer"></li>
+                <li class="Last">
+                    <a id="MainContent_lnkPagination5" class="5 " href="javascript:void(0);" target="_self">
+                        <span class="Arrow"></span>
+                        <span class="Inner">
+                            <img id="MainContent_imgPagination5" src="Assets/IMG/IconSohu.png" alt="We are hiring" />
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="Break80"></div>
+
+        <div class="WorkArea">
+            <ul>
+                <li><a href=""><img src="images/img_shot1.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+                <li><a href=""><img src="images/img_shot2.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+                <li><a href=""><img src="images/img_shot3.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+                <li><a href=""><img src="images/img_shot4.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+                <li><a href=""><img src="images/img_shot5.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+                <li><a href=""><img src="images/img_shot6.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+                <li><a href=""><img src="images/img_shot7.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+                <li><a href=""><img src="images/img_shot8.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+                <li><a href=""><img src="images/img_shot9.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+                <li><a href=""><img src="images/img_shot10.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+                <li><a href=""><img src="images/img_shot11.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+                <li><a href=""><img src="images/img_shot12.jpg" alt="" /><div class="mask"><i class="iconScale"></i></div></a></li>
+            </ul>
+        </div>
+
+        <!-- <a href="javascript:void(0);" class="StatsPrevArrow"> <span class="Hidden">上一个</span> </a> 
+        <a href="javascript:void(0);" class="StatsNextArrow"> <span class="Hidden">下一个</span> </a> 
+
+        <div class="StatsArea">
+            <ul>
+                <li>
+                    <img id="MainContent_Stats1_imgStat1" src="Assets/IMG/Stat1.jpg" alt="Stat 1" />
+                </li>
+                <li>
+                    <img id="MainContent_Stats1_imgStat2" src="Assets/IMG/Stat2.jpg" alt="Stat 2" />
+                </li>
+                <li>
+                    <img id="MainContent_Stats1_imgStat3" src="Assets/IMG/Stat3.jpg" alt="Stat 3" />
+                </li>
+                <li>
+                    <img id="MainContent_Stats1_imgStat4" src="Assets/IMG/Stat4.jpg" alt="Stat 4" />
+                </li>
+                <li>
+                    <img id="MainContent_Stats1_imgStat5" src="Assets/IMG/Stat5.jpg" alt="Stat 5" />
+                </li>
+            </ul>
+        </div> -->
+
+        <div class="HR"></div>
+        <h1 class="FloatLeft IconStar"></h1>
+        <div class="Clear"></div>
+        <div class="ClientGrid">
+            <div class="BoxGrid">
+                <ul>
+                    <li class="First">
+                        <img src="Assets/IMG/service_1.gif"/>
+                        <div class="Border"></div>
+                        <p> 政府行政审批管理系统，高速路收费系统 企业仓库管理系统，企业进销管理 企业员工管理（OA），企业费用管理系统 </p> 
+                    </li>
+                    <li>
+                        <img src="Assets/IMG/service_2.gif"/>
+                        <div class="Border"></div>
+                        <p> iPhone/iPad/安卓客户端设计、开发、 发布、版本维护， 也可与传统软件对接，移动办公 </p> 
+                    </li>
+                    <li>
+                        <img src="Assets/IMG/service_3.gif"/>
+                        <div class="Border"></div>
+                        <p> 最前卫WEB开发技术。一个网站，解决手机设备，普通电脑观看的问题。 响应设计，自动匹配浏览终端 拥抱移动互联网，让您的网站更出众 </p> 
+                    </li>
+                    <li>
+                        <img src="Assets/IMG/service_4.gif"/>
+                        <div class="Border"></div>
+                        <p> 网店日常活动，重大节假日，专题设计/制作。 产品 微博推广，海外推广。 </p> 
+                    </li>
+                </ul>
+                <div class="Clear"></div>
+            </div>
+        </div>
+        <div class="HR" style="height:64px;"></div>
+        <div class="TagBox">
+            <a href="" class="tagStyle">响应式网站设计</a>
+            <a href="" class="tagStyle">手机站点设计</a>
+            <a href="" class="tagStyle">flash交互网站</a>
+            <a href="" class="tagStyle">html5&css3</a>
+            <a href="" class="tagStyle">前、后端开发</a>
+            <a href="" class="tagStyle">搜索引擎优化</a>
+            <a href="" class="tagStyle">数据分析</a>
+            <a href="" class="tagStyle">图形设计</a>
+            <a href="" class="tagStyle">品牌设计</a>
+            <a href="" class="tagStyle">品牌维护</a>
+            <a href="" class="tagStyle">产品宣传</a>
+            <a href="" class="tagStyle">海外推广宣传</a>
+            <a href="" class="tagStyle">邮件营销、互动</a>
+            <div class="Clear"></div>
+        </div>
+    </div>
+</div>
+<!-- ==内容区域== -->
+
+<div class="Footer">
+    <div class="Wrapper">
+    <div class="FloatLeft"><span>2014 © </span>版权所有 湖北天地-专业的产品体验设计解决机构</div>
+    <div class="FloatRight">
+        <a href=""><i class="icon-mail"></i></a>
+        <a href=""><i class="icon-pin"></i></a>
+        <a href=""><i class="icon-linkedin"></i></a>
+        <a href=""><i class="icon-github"></i></a>
+        <a href=""><i class="icon-weibo"></i></a>
+        <a href=""><i class="icon-behance"></i></a>
+        <a href=""><i class="icon-dribbble"></i></a>
+    </div>
+    <div class="Clear"></div>
+    
+<!--         <div class="FloatLeft">
+            <h1 class="HasSubHeader">做为同路人，我们深刻理解</h1>
+            <h3 class="SubHeader">优秀的体验设计能够为事业成功注入一份信心！</h3>
+        </div>
+
+        <div class="FloatRight">
+            <span class="Telephone">+86 (0) 1552 7301 701</span>
+            <a href="mailto:sun.firebamboo@gmail.com" target="_self" class="Email">sun.firebamboo@gmail.com</a>
+        </div>
+
+        <div class="Clear"></div>
+        <div class="HR"></div>
+
+        <div class="SocialColumn First">
+            <div id="upNewsletter">
+                <h2>邮箱</h2>
+                <div id="pnlNewsletter">
+
+                    <div class="TextboxFrame">
+                        <input name="ctl00$txtNewsletter" type="text" value="请输入邮箱地址" id="txtNewsletter" title="请输入邮箱地址" class="Textbox" />
+                    </div>
+                    <div class="Break15"></div>
+                    <a id="lnkNewsletter" class="Button FullWidth" href="javascript:__doPostBack(&#39;ctl00$lnkNewsletter&#39;,&#39;&#39;)">
+                        <span> <span> <span class="IconArrow">提 交</span> </span> </span> 
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="SocialColumn">
+            <h2>社交</h2>
+            <div class="DarkFrame SocialIcons">
+                <a href="http://www.facebook.com/mobedia" target="_blank" class="Facebook">
+                    <span class="Hidden">Facebook</span>
+                </a>
+                <a href="https://twitter.com/mobedia" target="_blank"class="Twitter"> 
+                    <span class="Hidden">Twitter</span>
+                </a>
+                <a href="http://vimeo.com/mobedia" target="_blank" class="Vimeo"> 
+                    <span class="Hidden">behance</span>
+                </a>
+
+                <a href="http://dribbble.com/savman" target="_blank" class="Dribble">
+                    <span class="Hidden">Dribbble</span>
+                </a>
+                <div class="Clear"></div>
+            </div>
+        </div>
+
+        <div class="SocialColumn">
+            <h2 class="FloatLeft">微博</h2>
+            <div class="FloatRight" style="margin-top: -5px;">
+                <a href="https://twitter.com/mobedia" target="_blank" class="Button SmallButton">
+                    <span> <span>Follow&nbsp;&nbsp;+</span> </span> 
+                </a>
+            </div>
+            <div class="Clear"></div>
+            <div class="Tweets"></div>
+            <div class="TwitterFeed Hidden">
+                <ul id="twitter_update_list">
+                    <!-- <script src="http://twitter.com/javascripts/blogger.js" type="text/javascript"></script>
+                <script src="https://api.twitter.com/1/statuses/user_timeline.json?screen_name=mobedia&callback=twitterCallback2&count=6"
+                                            type="text/javascript"></script>
+  
+                </ul>
+            </div>
+        </div>
+
+        <div class="SocialColumn">
+            <h2 class="FloatLeft">地图</h2>
+            <div class="FloatRight" style="margin-top: -5px;">
+                <a id="lnkFindUs" class="Button SmallButton" href="https://maps.google.co.uk/maps?hl=en&q=SK9+7BQ&ie=UTF-8&hq=&hnear=0x487a4dede67caa15:0xf03fd3342132ba55,SK9+7BQ&gl=uk&ei=wSpbUJWmOunL0QWwwICYBA&ved=0CF8QtgM" target="_blank">
+                <span> <span>View Map</span> </span> 
+                </a>
+            </div>
+            <div class="Clear"></div>
+
+            <p class="Address">
+                <span class="White">滔滔</span> <br /> 湖北省襄阳市, <br /> 樊城区建华路152号， <br /> <em>始创于1992</em> 
+            </p>
+        </div> -->
+    </div>
+    <!-- <div class="Break80"></div> -->
+</div>
+</form>
+</body>
+</html>
